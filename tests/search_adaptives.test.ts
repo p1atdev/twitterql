fe48b22afd95d80cab6f839c5402eee2620c0a67
p1atdev <shu_teiei@outlook.jp>
2022-05-05 15:16:16 +0900

    feat: update client

--- tests/search_adaptives.test.ts
+++ tests/search_adaptives.test.ts
@@ -1,10 +1,21 @@
-import { getSearchAdaptive } from "../src/twitter/mod.ts";
+import { TwitterQLClient } from "../mod.ts";
+import { SearchAdaptive } from "../src/param/mod.ts";
+import { SearchAdaptiveParam, SearchAdaptiveRes } from "../src/types/mod.ts";
 import { assertNotEquals } from "../deps.ts";
 
 Deno.test("search adaptive", async () => {
-  const result = await getSearchAdaptive({
+  const client = new TwitterQLClient();
+
+  await client.setup();
+
+  const param: SearchAdaptiveParam = {
     q: "from:@twitter",
     count: 1,
+  };
+
+  const result = await client.request<SearchAdaptiveRes>({
+    endpoint: SearchAdaptive,
+    variables: param,
   });
 
   assertNotEquals(result.timeline.instructions.length, 0);
