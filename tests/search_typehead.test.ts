84828c3bc00cedf17c06005ac8b4c702a88745bb
p1atdev <shu_teiei@outlook.jp>
2022-05-05 15:16:16 +0900

    feat: update client

--- tests/search_typehead.test.ts
+++ tests/search_typehead.test.ts
@@ -1,19 +1,37 @@
-import { getSearchTypehead } from "../src/twitter/mod.ts";
+import { TwitterQLClient } from "../mod.ts";
+import { SearchTypehead } from "../src/param/mod.ts";
+import { SearchTypeheadParam, SearchTypeheadRes } from "../src/types/mod.ts";
 import { assertEquals, assertNotEquals } from "../deps.ts";
 
 Deno.test("search typehead", async () => {
-  const results = await getSearchTypehead({
+  const client = new TwitterQLClient();
+  await client.setup();
+
+  const param: SearchTypeheadParam = {
     q: "@twitter",
     result_type: "users",
+  };
+
+  const results = await client.request<SearchTypeheadRes>({
+    endpoint: SearchTypehead,
+    variables: param,
   });
 
   assertNotEquals(results.users.length, 0);
 });
 
 Deno.test("search typehead (shadowbanned)", async () => {
-  const shadowBanned = await getSearchTypehead({
+  const client = new TwitterQLClient();
+  await client.setup();
+
+  const param: SearchTypeheadParam = {
     q: "@ojitan_55",
     result_type: "users",
+  };
+
+  const shadowBanned = await client.request<SearchTypeheadRes>({
+    endpoint: SearchTypehead,
+    variables: param,
   });
 
   assertEquals(shadowBanned.users.length, 0);
