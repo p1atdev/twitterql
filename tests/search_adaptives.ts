4663d8fb7195d445d0e171922026bb0bf4adbe30
p1atdev <shu_teiei@outlook.jp>
2022-05-05 14:07:25 +0900

    chore: update test

--- tests/search_adaptives.ts
+++
@@ -1,8 +0,0 @@
-import { getSearchAdaptive } from "../src/twitter/mod.ts";
-
-const result = await getSearchAdaptive({
-  q: "from:@RyogaooWW",
-  count: 1,
-});
-
-console.dir(result, { depth: 20 });
